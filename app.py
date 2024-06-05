1b53bcc17cd9d07afd25a1aad022635431ae70f5
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-06-05 14:49:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ def contentgenerator_ai():
 #     #    retriever = retriever_existingdb()
     #    return query_llm(retriever, queryfromfe)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
+    
     
     
