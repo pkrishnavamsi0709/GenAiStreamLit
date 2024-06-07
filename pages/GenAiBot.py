033028541a13e5ef155fbfb2e254d5045ba0d6f7
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-06-07 15:20:01 +0530

    Update GenAiBot.py

--- pages/GenAiBot.py
+++ pages/GenAiBot.py
@@ -3,11 +3,11 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain import PromptTemplate
-import speech_recognition as sr
-import pyaudio
  
+load_dotenv()  # take environment variables from .env.
+api_key = os.getenv("GEMINI_API_KEY")
 model = genai.GenerativeModel('gemini-pro')
-genai.configure(api_key="AIzaSyDcF1LrSLzb9l3B7NfS_5LFNyoGnMv6K_g")
+genai.configure(api_key=api_key)
  
 st.set_page_config(page_title="Chatbot", page_icon="🤖")
  
@@ -53,42 +53,12 @@ def askgenaibot(input):
                               safety_settings=safety_settings)
     response = model.generate_content(f"Generate Content {input}")
     return response.text
+
+input = st.text_input("Input Prompt: ",key="input")
+prompt_formatted_str: str = prompt.format(question=input)
+if st.button("search"):
+    response=askgenaibot(prompt_formatted_str)
+    st.subheader("The Response is")
+    st.write(response)
  
-def speech_to_text():
-    recognizer = sr.Recognizer()
- 
-    with sr.Microphone() as source:
-        st.write("Speak something...")
-        # Adjust for ambient noise
-        recognizer.adjust_for_ambient_noise(source)
-        # Listen for audio input
-        audio = recognizer.listen(source)
-    try:
-        # Use Google Speech Recognition
-        text = recognizer.recognize_google(audio)
-        st.write("Your input:", text)
-        return text
-    except sr.UnknownValueError:
-        st.write("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
-speak=""
-with st.sidebar:
-    st.header("Chat through voice 🎤")
-    if st.button("speak"):        
-        text = speech_to_text()
-        speak = text
-        if text:
-                response = model.generate_content(text)
-                st.write(f"Translation: {response.text}")
-        else:
-            st.write("Please enter text to Genrate.")
- 
-if speak is not None:
-    input = st.text_input("Input Prompt: ",key="input")
-    prompt_formatted_str: str = prompt.format(question=input)
-    if st.button("search"):
-        response=askgenaibot(prompt_formatted_str)
-        st.subheader("The Response is")
-        st.write(response)
  
