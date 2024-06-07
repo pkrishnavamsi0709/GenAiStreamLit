761d10443d740ae28e7ebb3a969c3bf472786412
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-06-07 15:00:06 +0530

    Update Translator.py

--- pages/Translator.py
+++ pages/Translator.py
@@ -1,33 +1,32 @@
 import os
-import streamlit as st
 import google.generativeai as genai
-
-# Configure the Generative AI API
-genai.configure(api_key="AIzaSyDcF1LrSLzb9l3B7NfS_5LFNyoGnMv6K_g")
-
-# Create the GenerativeModel instance
-model = genai.GenerativeModel('gemini-1.5-pro-latest')
-
-# Streamlit app
-def main():
-    st.title("AI Translator")
-
-    # Input fields for source language, target language, and text
-    sourcelanguage = st.text_input("Enter source language:")
-    targetlanguage = st.text_input("Enter target language:")
-    text = st.text_area("Enter text to translate:")
-
-    # Translate button
-    if st.button("Translate"):
-        if sourcelanguage.strip() == '' or targetlanguage.strip() == '' or text.strip() == '':
-            st.error("Please fill in all fields.")
-        else:
-            # Generate translation
-            response = model.generate_content(f"Translate the following sentence from language {sourcelanguage} to language {targetlanguage}: {text}")
-            translated_text = response.text
-
-            # Display translated text
-            st.success(f"Translated text: {translated_text}")
-
-if __name__ == "__main__":
-    main()
+import streamlit as st
+ 
+api_key = os.getenv("AIzaSyDcF1LrSLzb9l3B7NfS_5LFNyoGnMv6K_g")
+genai.configure(api_key=api_key)
+ 
+st.set_page_config(page_title="Language Translator", page_icon="🌐")
+st.header("Translator 📖")
+ 
+model = genai.GenerativeModel('gemini-pro')
+text = st.text_area("Enter text to translate:")
+ 
+source_language = st.selectbox("Source Language", ["English", "French", "Spanish", "German"])
+target_language = st.selectbox("Target Language", ["French", "chinese", "Arabic", "Spanish", "German"])
+ 
+language_codes = {
+    "English": "en",
+    "chinese": "ch",
+    "French": "fr",
+    "Spanish": "es",
+    "German": "de",
+    "Arabic":"ar",
+}
+ 
+if st.button("Translate"):
+    if text:
+        response = model.generate_content(f"Translate the following sentence from language code {language_codes[source_language]} to {language_codes[target_language]}: {text}")
+        st.write(f"Translation: {response.text}")
+    else:
+        st.write("Please enter text to translate.")
+ 
