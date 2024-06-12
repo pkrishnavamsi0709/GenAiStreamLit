e0199836b8cf24e4a330b7d98582790f9e357861
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-06-12 10:32:22 +0530

    Update GenAiBot.py

--- pages/GenAiBot.py
+++ pages/GenAiBot.py
@@ -11,7 +11,7 @@ genai.configure(api_key=api_key)
  
 st.set_page_config(page_title="Chatbot", page_icon="🤖")
  
-st.header("Chatbot 🤖")
+st.header("Chat Interface for Text Generation")
  
 generation_config = {
   "temperature": 0.9,
