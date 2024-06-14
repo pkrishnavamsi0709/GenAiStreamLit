7785d1ad740569c495d17502e320cc5eb13d3d67
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-06-14 11:15:37 +0530

    Update and rename Image_To_Text_Bot.py to Image_To_Text_Description.py

--- pages/Image_To_Text_Description.py
+++ pages/Image_To_Text_Description.py
@@ -10,7 +10,7 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
  
-st.set_page_config(page_title="Image Description Interface Using AI Model", page_icon="📸")
+st.set_page_config(page_title="Description of Products", page_icon="📸")
  
 prompt_template: str = """/
 Use the following pieces of context to answer the question/
@@ -31,8 +31,7 @@ def get_gemini_response(prompt_formatted_str,image):
        response = model.generate_content(image)
     return response.text
  
-# st.header("Image Description Interface Using AI Model")
-st.header("Image to Text Bot")
+st.header("Image to Text Descripton")
 # input = st.text_input("Input Prompt: ",key="input")
 input = "describe the object"
 prompt_formatted_str: str = prompt.format(
